f7a9d148d8b07a76f0d4aa7dc23766d631c1db65
alirehman2210 <alirehman2210@gmail.com>
2025-06-01 16:19:24 +0200

    Update ESLint configuration and build command

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: 'next/core-web-vitals',
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/no-unescaped-entities': 'off',
+    'no-unused-vars': 'off'
+  }
+} 
